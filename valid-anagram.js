8e26a8eada792f65d8b5cfa88da235f7b02a09c8
OsamaDW <s12028609@stu.najah.edu>
2025-03-04 01:45:51 +0200

    Valid Anagram

--- valid-anagram.js
+++ valid-anagram.js
@@ -3,6 +3,33 @@
  * @param {string} t
  * @return {boolean}
  */
+/*
+* old answer
+* */
 var isAnagram = function(s, t) {
     return (s.split("").sort().join("") === t.split("").sort().join(""))
+};
+
+/*
+* enhanced answer
+* */
+
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var countingSort = (array)=>{
+    let map = new Array(26).fill(0);
+    let base = 'a'.charCodeAt();
+    for(arr of array){
+        map[arr.charCodeAt() - base]++;
+    }
+    let orderdArray= "";
+    map.forEach((el, key) => orderdArray+=String.fromCharCode(key + base).repeat(el));
+
+    return orderdArray;
+}
+var isAnagram = function(s, t) {
+    return (countingSort(s) === countingSort(t))
 };
